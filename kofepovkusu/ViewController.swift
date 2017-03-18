1a81cb4f500a6dae8444b1237a6eb3c75af247e7
Evgeny Sugakov <shiroginne@gmail.com>
2017-03-18 12:03:53 +0100

    Initial Commit

---
+++ kofepovkusu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kofepovkusu
+//
+//  Created by Suhakou Yauheni on 18/03/2017.
+//  Copyright © 2017 kaff.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
